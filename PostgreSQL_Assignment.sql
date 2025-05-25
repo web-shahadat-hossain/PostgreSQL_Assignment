7005d36fa7f1e2ec0607388a103d154f59e73105
Md Shahadat Hossain <mdridoysikder862@gmail.com>
2025-05-25 12:38:11 +0600

    Tables is created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,2 +1,26 @@
 -------------- Create a database --------------
 CREATE DATABASE conservation_db;
+
+-------------- Create a Table --------------
+CREATE TABLE rangers (
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(80) NOT NULL,
+    region VARCHAR(100) NOT NULL
+);
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(80) NOT NULL,
+    scientific_name VARCHAR(80) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(100) NOT NULL
+);
+CREATE TABLE species (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INT NOT NULL REFERENCES rangers(ranger_id),
+    species_id INT NOT NULL REFERENCES species(species_id),
+    sighting_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    location TEXT NOT NULL,
+    notes TEXT
+);
+
+
