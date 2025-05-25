561750698787c3f87ec5e9aef976685af91197a3
Md Shahadat Hossain <mdridoysikder862@gmail.com>
2025-05-25 19:12:59 +0600

    Questions and is done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -25,54 +25,46 @@ CREATE TABLE sightings (
 
 
 -- ===============================================
--- Problem 1️⃣: 
--- Description: Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains
+-- Problem 1️⃣
 -- ===============================================
 INSERT INTO rangers (name, region) VALUES
 ('Derek Fox', 'Coastal Plains');
 
 -- ===============================================
--- Problem 2️⃣: 
--- Description: Count unique species ever sighted.
+-- Problem 2️⃣
 -- =============================================== 
 SELECT count(*) as unique_species_count FROM (SELECT species_id FROM sightings GROUP BY species_id);
 
 
 -- ===============================================
--- Problem 3️⃣: 
--- Description:  Find all sightings where the location includes "Pass".
+-- Problem 3️⃣
 -- =============================================== 
 SELECT * FROM sightings WHERE location ILIKE '%Pass%';
 
 -- ===============================================
--- Problem 4️⃣: 
--- Description: List each ranger's name and their total number of sightings.
+-- Problem 4️⃣
 -- =============================================== 
 SELECT r.name, count(s.ranger_id) as total_sightings  FROM sightings as s  JOIN rangers as r ON s.ranger_id = r.ranger_id GROUP BY r.name ; 
 
 
 -- ===============================================
--- Problem 5️⃣: 
--- Description: List species that have never been sighted.
+-- Problem 5️⃣
 -- =============================================== 
 SELECT *  FROM species WHERE species_id NOT IN (SELECT species_id FROM sightings);
 
 -- ===============================================
--- Problem 6️⃣: 
--- Description:  Show the most recent 2 sightings.
+-- Problem 6️⃣
 -- =============================================== 
 SELECT * FROM sightings ORDER BY sighting_time DESC LIMIT 2;
 
 
 -- ===============================================
--- Problem 7️⃣: 
--- Description:  Update all species discovered before year 1800 to have status 'Historic'.
+-- Problem 7️⃣
 -- =============================================== 
 UPDATE  species set conservation_status ='Historic' WHERE extract(YEAR FROM discovery_date::Date) < 1800;
 
 -- ===============================================
--- Problem 8️⃣: 
--- Description: Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
+-- Problem 8️⃣
 -- =============================================== 
 CREATE OR REPLACE FUNCTION get_the_time_of_day(p_time TIMESTAMP)
 RETURNS TEXT
@@ -94,8 +86,7 @@ SELECT sighting_id, get_the_time_of_day(sighting_time)  AS time_of_day  FROM sig
 
 
 -- ===============================================
--- Problem 9️⃣: 
--- Description: Delete rangers who have never sighted any species
+-- Problem 9️⃣
 -- =============================================== 
 DELETE FROM rangers WHERE ranger_id NOT IN(SELECT ranger_id FROM sightings)
 
